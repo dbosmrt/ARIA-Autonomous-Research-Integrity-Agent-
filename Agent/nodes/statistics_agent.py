e2650a930368a6b31d260309f227b64e6038d421
Deepanshu Bhatt <deepanshu.2006bhatt@gmail.com>
2026-06-13 17:33:11 +0530

    [Creating] Orchestrator / Statistics Agent

---
+++ Agent/nodes/statistics_agent.py
@@ -0,0 +1,14 @@
+"""
+Implementation of the Statistics Agent.
+-Checks for the statistical validity of the paper.
+-Checks for various questionable research practices.
+-Uses Statistical Tools.
+- Questionable research practices (QRPs)
+    - p-hacking
+    - HARKing
+    - selective reporting
+    - data dredging
+    - outcome switching
+    - double dipping
+    - fraud and misconduct
+"""
---
+++ Agent/orchestrator.py
@@ -0,0 +1,5 @@
+"""
+Orchestrator Agent. 
+Manages the agents and the workflow of the system.
+Manages the agents based on the type of paper, and what sources we need.
+"""
